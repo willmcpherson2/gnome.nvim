7e9f4aa1f5545b5f989e24f3a64d902b8564a1e1
William McPherson <willmcpherson2@gmail.com>
2022-12-17 17:47:23 +1100

    Add options with defaults

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # gnome.nvim
 
-A tiny Neovim plugin that switches between `background=light` and
-`background=dark` based on your GNOME settings.
+A Neovim plugin for switching themes based on GNOME settings.
 
 Integrates well with the [GNOME Night Theme Switcher
 extension](https://extensions.gnome.org/extension/2236/night-theme-switcher/).
@@ -11,7 +10,15 @@ extension](https://extensions.gnome.org/extension/2236/night-theme-switcher/).
 use {
   "willmcpherson2/gnome.nvim",
   config = function()
-    require("gnome").init()
+    require("gnome").init {
+      -- these are the default options and can be omitted
+      on_light = function()
+        vim.api.nvim_set_option("background", "light")
+      end,
+      on_dark = function()
+        vim.api.nvim_set_option("background", "dark")
+      end,
+    }
   end
 }
 ```
--- lua/gnome/init.lua
+++ lua/gnome/init.lua
@@ -1,15 +1,26 @@
 local cmd = "gsettings monitor org.gnome.desktop.interface color-scheme"
 
-local function on_stdout(_, data, _)
-  if data[1] == "color-scheme: 'prefer-dark'" then
-    vim.api.nvim_set_option("background", "dark")
-  else
-    vim.api.nvim_set_option("background", "light")
+local function on_stdout(options)
+  return function(_, data, _)
+    if data[1] == "color-scheme: 'prefer-dark'" then
+      options.on_dark()
+    else
+      options.on_light()
+    end
   end
 end
 
-local function init()
-  vim.fn.jobstart(cmd, { on_stdout = on_stdout })
+local function init(options)
+  setmetatable(options, { __index = {
+    on_light = function()
+      vim.api.nvim_set_option("background", "light")
+    end,
+    on_dark = function()
+      vim.api.nvim_set_option("background", "dark")
+    end,
+  } })
+
+  vim.fn.jobstart(cmd, { on_stdout = on_stdout(options) })
 end
 
 return { init = init }
